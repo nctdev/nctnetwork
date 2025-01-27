2bb89f20ff12cda942e4e4f7cad8ecac078e89a5
nctdev <dave@nctnetwork.co.uk>
2025-01-27 13:43:42 +0000

    v1.0.1

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,13 +8,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
-    },
-    {
-      url: 'https://nctnetwork.co.uk/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    // Add more routes as needed
+    },    // Add more routes as needed
   ]
 }
