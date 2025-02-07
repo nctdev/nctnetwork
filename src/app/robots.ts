1d50101de02113f1dcd4edddcdeef7c392d19355
nctdev <dave@nctnetwork.co.uk>
2025-02-07 08:27:47 +0000

    v1.0.2

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,26 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/'], // Add any private paths you want to block
-    },
-    sitemap: 'https://nctnetwork.co.uk/sitemap.xml'
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/sitemap.xml', '/_next/static/'],
+        disallow: ['/api/', '/private/', '/admin/', '/_next/image*'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: ['/', '/*.js', '/*.css'],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: ['/', '/*.js', '/*.css'],
+      },
+    ],
+    // Optionally add a crawl delay (in seconds)
+    crawlDelay: 1,
+    // Path to your sitemap
+    sitemap: 'https://nctnetwork.co.uk/sitemap.xml',
+    // Host directive is optional but recommended
+    host: 'https://nctnetwork.co.uk',
   }
 }
