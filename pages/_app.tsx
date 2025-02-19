df8ff89c45c0d001ad07766688f9cbc3ff1afe9f
nctdev <dave@nctnetwork.co.uk>
2025-02-19 11:45:44 +0000

    v1.0.17

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'; // Adjust the path if your CSS file is located elsewhere
 import { ThemeProvider } from 'next-themes'; // If you're using next-themes
+import type { AppProps } from 'next/app'; // Import AppProps
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) { // Use AppProps for typing
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <Component {...pageProps} />
